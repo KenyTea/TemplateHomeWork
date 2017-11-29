ea3e68c5596b5ae980dfa6d10786b419e387d20b
Васильева <@223_16>
2017-11-29 19:34:07 +0600

    Структурируем стэк

---
+++ Steck 29.11.17/DoubleLinkList.cpp
@@ -0,0 +1,13 @@
+#include "stdafx.h"
+#include "DoubleLinkList.h"
+
+template<class T>
+DoubleLinkList <T>::DoubleLinkList()
+{
+
+}
+
+template<class T>
+DoubleLinkList <T>::~DoubleLinkList()
+{
+}
---
+++ Steck 29.11.17/DoubleLinkList.h
@@ -0,0 +1,9 @@
+#pragma once
+template<class T>
+class DoubleLinkList
+{
+public:
+	DoubleLinkList();
+	~DoubleLinkList();
+};
+
