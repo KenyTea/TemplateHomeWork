853bc8913058e4eb33c7cd9ef43d09fdba0761cd
Васильева <@223_16>
2017-12-20 19:33:35 +0600

    Map

--- 20.12.17/20.12.17.cpp
+++ 20.12.17/20.12.17.cpp
@@ -1,15 +1,31 @@
 //итераторы iterators
 #include <iostream>
 #include <vector>
-
-
-int main() {
+#include <map>
+#include <string>
+void Vector() {
 	int arr[10] = { 1,2,3,4,5,6,7,8,9,0 };
 	std::vector <int> x(arr, arr + 10);
 	std::vector <int> ::iterator it = x.begin(); // iterator необходим для бега по массиву, чтоб не вышел из деапазона
 	for (; it != x.end(); ++it)
 	{
 		std::cout << *it << '\t';
+	}
+	std::cout << std::endl;
+
+}
+void Map() {
+
+}
+int main() {
+	
+
+	std::map <std::string, int> dictionary;
+	dictionary.emplace(std::make_pair<std::string, int>("123", 1));
+	std::cout << dictionary.at("123") << std::endl;
+	auto it2 = dictionary.begin();
+	for (; it2 != dictionary.end(); ++it2) {
+		std::cout << it2->first << '\t' << it2->second << '\t';
 	}
 	std::cout << std::endl;
 
