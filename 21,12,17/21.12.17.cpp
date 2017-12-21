bbb1ce34df2e3bba345e12e5b1fc9bea8f298fd2
Васильева <@223_16>
2017-12-21 20:20:29 +0600

    Lambda - [y](A & a) {return a.value == y; }

--- 21,12,17/21.12.17.cpp
+++ 21,12,17/21.12.17.cpp
@@ -19,18 +19,42 @@ public:
 	int value;
 	friend std::ostream & operator << (std::ostream &, const A &);
 	friend void operator >> (std::istream & stream, const A & a);
+
+	void Show() {
+
+		std::cout << value << std::endl;
+	}
+
 };
 std::ostream & operator << (std::ostream & stream, const A & a) {
 	return stream << a.value;
 }
 void operator >> (std::istream & stream, const A & a) {
 	stream >> a;
-}
+} 
+
 
 
 int main() {
 
 	std::vector<A> x = { A(1), A(2) ,A(3) };
+	x.push_back(A(6));
+	x.shrink_to_fit();
+	x.pop_back();
+	x.pop_back();
+	std::cout << x.size() << std::endl;
+	std::cout << x.max_size() << std::endl;
+	int y;
+	std::cin >> y;
+	
+	auto a = std::find_if(x.begin(), x.end(), [y](A & a) {return a.value == y; }); // Lambda - [y](A & a) {return a.value == y; }
+	std::cout << std::endl;
 
+	if (a != x.end()) {
+		std::cout << *a << '\n';
+	}
+	else std::cout << "not found" << std::endl;
+
+	system("pause");
 	return 0;
 }
