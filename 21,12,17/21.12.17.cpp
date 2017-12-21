a8967214a4171853581714e311c83bbf5716213c
Васильева <@223_16>
2017-12-21 20:49:15 +0600

    +Версия Муслима

--- 21,12,17/21.12.17.cpp
+++ 21,12,17/21.12.17.cpp
@@ -7,54 +7,110 @@
 #include <unordered_map>
 
 // lymbda
+//class A {
+//public:
+//	A() {
+//		std::cout << "A()" << std::endl;
+//	}
+//	A(int x): value(x){}
+//	~A() {
+//		std::cout << "~A()" << std::endl;
+//	}
+//	int value;
+//	friend std::ostream & operator << (std::ostream &, const A &);
+//	friend void operator >> (std::istream & stream, const A & a);
+//
+//	void Show() {
+//
+//		std::cout << value << std::endl;
+//	}
+//
+//};
+//std::ostream & operator << (std::ostream & stream, const A & a) {
+//	return stream << a.value;
+//}
+//void operator >> (std::istream & stream, const A & a) {
+//	stream >> a;
+//} 
+//
+//
+//
+//int main() {
+//
+//	std::vector<A> x = { A(1), A(2) ,A(3) };
+//	x.push_back(A(6));
+//	x.shrink_to_fit();
+//	x.pop_back();
+//	x.pop_back();
+//	std::cout << x.size() << std::endl;
+//	std::cout << x.max_size() << std::endl;
+//	int y;
+//	std::cin >> y;
+//	
+//	auto a = std::find_if(x.begin(), x.end(), [y](A & a) {return a.value == y; }); // Lambda - [y](A & a) {return a.value == y; }
+//	std::cout << std::endl;
+//
+//	if (a != x.end()) {
+//		std::cout << *a << '\n';
+//	}
+//	else std::cout << "not found" << std::endl;
+//
+//	system("pause");
+//	return 0;
+//}
+
+
+// Lambda.cpp : Defines the entry point for the console application.
+//
+
+
+
 class A {
 public:
 	A() {
 		std::cout << "A()" << std::endl;
 	}
-	A(int x): value(x){}
+	A(int x) : value(x) {
+		std::cout << "A(int)" << std::endl;
+	}
 	~A() {
 		std::cout << "~A()" << std::endl;
 	}
 	int value;
-	friend std::ostream & operator << (std::ostream &, const A &);
-	friend void operator >> (std::istream & stream, const A & a);
-
-	void Show() {
-
-		std::cout << value << std::endl;
-	}
-
+	friend std::ostream & operator<<(std::ostream &, const A &);
+	friend void operator>>(std::istream & stream, const A & a);
 };
-std::ostream & operator << (std::ostream & stream, const A & a) {
+std::ostream & operator<<(std::ostream & stream, const A & a)
+{
 	return stream << a.value;
 }
-void operator >> (std::istream & stream, const A & a) {
+void operator>>(std::istream & stream, const A & a) {
 	stream >> a;
-} 
+}
+class Finder {
+public:
+	Finder(int y) : x(y) {}
+	int x;
+	bool operator()(const A & a) {
+		return x == a.value;
+	}
+};
 
-
-
-int main() {
-
-	std::vector<A> x = { A(1), A(2) ,A(3) };
-	x.push_back(A(6));
-	x.shrink_to_fit();
-	x.pop_back();
-	x.pop_back();
-	std::cout << x.size() << std::endl;
-	std::cout << x.max_size() << std::endl;
+int main()
+{
+	std::vector<A> x = { A(1), A(2), A(3) };
+	std::for_each(x.begin(), x.end(), [](A a) { std::cout << a.value << '\t'; });
+	std::cout << std::endl;
 	int y;
 	std::cin >> y;
-	
-	auto a = std::find_if(x.begin(), x.end(), [y](A & a) {return a.value == y; }); // Lambda - [y](A & a) {return a.value == y; }
-	std::cout << std::endl;
-
+	Finder f(y);
+	auto a = std::find_if(x.begin(), x.end(), f);
+	auto b = std::find_if(x.begin(), x.end(), [y](A & a) {return a.value == y; });
 	if (a != x.end()) {
-		std::cout << *a << '\n';
+		std::cout << *a << '\t' << *b << std::endl;
 	}
-	else std::cout << "not found" << std::endl;
-
-	system("pause");
+	else {
+		std::cout << "not found" << std::endl;
+	}
 	return 0;
 }
