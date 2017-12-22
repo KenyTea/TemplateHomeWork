73cd746d26bded7925cf3e7657e3c406beed63d9
Васильева <@223_16>
2017-12-22 20:42:36 +0600

    min element whithout functor sort 0 - 9 sort 9 - 0

--- 22.12.17/22.12.17.cpp
+++ 22.12.17/22.12.17.cpp
@@ -7,6 +7,7 @@
 #include <unordered_map>
 #include <array>
 #include <time.h>
+#include <functional>
 
 
 class A { // functor
@@ -21,10 +22,10 @@ int main() {
 	
 	std::for_each(x.begin(), x.end(), [](int & a) {a = rand() % 10; std::cout << a << " "; }); std::cout << std::endl; // set random numbers in vector
 	
-	std::cout << "Min element " << *std::min_element(x.begin(), x.end(), a); std::cout << std::endl; // min element
+	std::cout << "Min element " << *std::min_element(x.begin(), x.end()/*, a*/); std::cout << std::endl; // min element whithout functor
 	std::cout << "Max element " << *std::max_element(x.begin(), x.end(), a); std::cout << std::endl; // max lement
-
-	
+	std::cout << "Greater " << *std::max_element(x.begin(), x.end(), std::greater<int>()); // sort 0 - 9
+	std::cout << "Less " << *std::max_element(x.begin(), x.end(), std::less<int>());// sort 9 - 0
 	system("pause");
 	return 0;
 }
