9603263b56a7833a5cf2fdec699df1df7df577ae
Alex <vassilyevalexey@gmail.com>
2018-01-12 19:47:18 +0600

    исправил

--- 10,01,18/10,01,18Progect.cpp
+++ 10,01,18/10,01,18Progect.cpp
@@ -31,7 +31,7 @@ enum ConsoleColor {
 	White = 15
 };
 
-class Question {
+class Question {// class for keep questtions, variants & correct answer
 public:
 	std::string content;
 	std::string variants[4];
@@ -41,23 +41,11 @@ public:
 class Quiz {
 	
 public:
-	std::string File(int temp) {
-		std::string var;
-		if (temp == 1)
-		{
-			var = "mathematics.txt";
-			return var;
-        }
-		else if (temp == 2) 
-		{
-			var = "PC.txt";
-			return var;
-		}
-	}
+	
 	std::vector<Question> questions; // vector whith questions
 	
-	void retrieveQuestions(int var) {
-		std::ifstream fin(File(var)); // create file and read it
+	void mathematics() {
+		std::ifstream fin("mathematics.txt"); // create file and read it
 		if (!fin.is_open())  // check file
 		{
 			std::cout << "The file not found" << std::endl;
@@ -78,9 +66,33 @@ public:
 				questions.push_back(q);
 			}
 		}
+		fin.close();
 	}
 
-	
+	void PC() {
+		std::ifstream fin("PC.txt"); // create file and read it
+		if (!fin.is_open())  // check file
+		{
+			std::cout << "The file not found" << std::endl;
+		}
+		else {
+			std::string temp; // creat temp string
+			while (!fin.eof()) {
+				getline(fin, temp); //read first string 
+				Question q; // creat Question's object
+				q.content = temp; // write the read string to the created variable for the question
+
+				for (int i = 0; i < 4; ++i) {
+					getline(fin, temp); // read variants 
+					q.variants[i] = temp; // wright variants
+				}
+				getline(fin, temp); // read right unswer
+				q.right = temp; // write unswer to temp
+				questions.push_back(q);
+			}
+		}
+		fin.close();
+	}
 };
 
 
@@ -89,6 +101,18 @@ public:
 	int counter = 0;
 	int choice;
 	std::string name;
+	std::string password;
+	void Registration() {
+		std::cout << "-----------REGOSTRATION-----------" << std::endl;
+		std::cout << "Please, enter your name ";
+		std::cin >> name;
+		std::system("cls");
+		std::cout << std::endl;
+		std::cout << "Please, enter password ";
+		std::cin >> password;
+
+
+	}
 	
 };
 
@@ -98,10 +122,9 @@ class App {
 public:
 	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
 	
-	
-	void doApp(int var) {
+	void AppMath() {
 		system("cls");
-		quiz.retrieveQuestions(var);
+		quiz.mathematics();
 		SetConsoleTextAttribute(hConsole, 3);
 		std::cout << "\tHelp\tEnter the variant number!!!" << std::endl;
 		SetConsoleTextAttribute(hConsole, 7);
@@ -149,6 +172,57 @@ public:
 
 
 	}
+
+	void AppPC() {
+		system("cls");
+		quiz.PC();
+		SetConsoleTextAttribute(hConsole, 3);
+		std::cout << "\tHelp\tEnter the variant number!!!" << std::endl;
+		SetConsoleTextAttribute(hConsole, 7);
+		std::cout << std::endl;
+		std::cout << player.name << std::endl;
+		for (int i = 0; i < quiz.questions.size(); ++i) {
+			std::cout << "\t" << quiz.questions[i].content << std::endl;
+			std::cout << std::endl;
+			for (int j = 0; j < 4; ++j) {
+				std::cout << "\t" << quiz.questions[i].variants[j] << std::endl;
+			}
+			std::cout << std::endl;
+			std::cout << "\tEnter your answer: ";
+			std::cin >> player.choice;
+			if (quiz.questions[i].variants[player.choice - 1] == quiz.questions[i].right) {
+				++player.counter;
+				SetConsoleTextAttribute(hConsole, 2);
+				std::cout << "\tCorrect";
+				SetConsoleTextAttribute(hConsole, 7);
+				_getch();
+				system("cls");
+			}
+			else {
+				std::cout << std::endl;
+				SetConsoleTextAttribute(hConsole, 12);
+				std::cout << "\tYour answer is    " << quiz.questions[i].variants[player.choice - 1] << std::endl;
+				SetConsoleTextAttribute(hConsole, 2);
+				std::cout << "\tCorrect answer is " << quiz.questions[i].right << std::endl;
+				SetConsoleTextAttribute(hConsole, 7);
+				_getch();
+				system("cls");
+				std::cout << std::endl;
+			}
+		}
+		if (player.counter > 5)
+		{
+			SetConsoleTextAttribute(hConsole, 2);
+			std::cout << "\tYour scor is " << player.counter << std::endl;
+			SetConsoleTextAttribute(hConsole, 7);
+		}
+		else
+			SetConsoleTextAttribute(hConsole, 12);
+		std::cout << "\tYour scor is " << player.counter << std::endl;
+		SetConsoleTextAttribute(hConsole, 7);
+
+
+	}
 };
 
 class Menu {
@@ -158,19 +232,23 @@ class Menu {
 	int var;
 public:
 	void menu1() {
-		std::cout << "To pass the test, enter your name ";
-		std::cin >> player.name;
-		std::cout << std::endl;
+		
 		system("cls");
+		std::cout << "----------------Menu--------------------" << std::endl;
+		std::cout << "If you want to pass a math test, enter 1" << std::endl;
+		std::cout << "If you want to pass a PC test, enter 2" << std::endl;
+		std::cout << "For EXIT, enter 0" << std::endl;
+		std::cout << std::endl;
+		std::cin >> var;
+		
 		while (true)
 		{
-			system("cls");
-			std::cout << "----------------Menu--------------------" << std::endl;
-			std::cout << "If you want to pass a math test, enter 1" << std::endl;
-			std::cout << "If you want to pass a PC test, enter 2" << std::endl;
-			std::cout << std::endl;
-			std::cin >> var;
-			app.doApp(var);
+			switch (var) {
+			case 1: app.AppMath(); break;
+			case 2: app.AppPC(); break;
+			case 0: return;
+			}
+			
 		}
 
 }
